56bbb4be29f7cdd25175f709b7a755b1c6f5e31b
Karlos <karin_ar72@hotmail.com>
2022-11-27 09:50:04 -0600

    Added functionality of Discuplina, Puntualidad, Trabajos y tareas, Examen and Total

--- main.js
+++ main.js
@@ -1,26 +1,34 @@
 //variables
 const CONCEPTO = document.getElementsByTagName('th');
-const TABLACANTIDAD = document.getElementsByClassName('tabla_cantidad_input');
-let TABLARESULTADO = document.getElementsByClassName('tabla_resultado');
+let cantidadAlumno = document.getElementsByClassName('tabla_cantidad_input');
+let resultado = document.getElementsByClassName('tabla_resultado');
 const TABLA = document.querySelector('tabla');
-const P_CANTIDAD = document.querySelector('#p_input_cantidad');
-const P_RESULTADO = document.querySelector('#p_input_resultado');
-const P_RESULTADO_NEGATIVO = document.querySelector('#p_input_resultado_negativo');
 const BOTON = document.querySelector('button');
 const TOTAL = document.querySelector('#total_resultado');
+let cantidadReal = document.getElementsByClassName('cantidad_real_input');
 
 //Eventos
 BOTON.addEventListener('click', comparacion);
 	function comparacion() {
-		let total = 0;
-		for(let i = 0; i < TABLACANTIDAD.length; i++) {
-			if(Number(TABLACANTIDAD[i].value) < Number(P_CANTIDAD.value)) {
-				TABLARESULTADO[i].innerText = Number(P_RESULTADO.value);
-			} else {
-				TABLARESULTADO[i].innerText = Number(P_RESULTADO_NEGATIVO.value);
-			}
-		total += Number(TABLARESULTADO[i].innerText);
+		//Resultado de Asistencias
+		Number(cantidadAlumno[0].value) <= Number(cantidadReal[0].value/2) ? resultado[0].textContent = Number(0.5) : resultado[0].textContent = Number(1);
 
-		};
+		//Resultado de Disciplina, Puntualidad y Examen
+		resultado[1].innerText = Number(cantidadAlumno[1].value);
+		resultado[2].innerText = Number(cantidadAlumno[2].value);
+		resultado[4].innerText = Number(cantidadAlumno[4].value);
+
+		//Resultado de Trabajos y tareas
+		Number(cantidadAlumno[3].value) <= Number(cantidadReal[1].value/2) 
+		? resultado[3].textContent = Number(1) 
+		: resultado[3].textContent = Number(2);
+
+		//Total
+		let total = 0;
+		for(let i = 0; i < resultado.length; i++) {
+			total += Number(resultado[i].innerText);
+		}
 		TOTAL.innerText = total;
-	}
+		}
+
+		
